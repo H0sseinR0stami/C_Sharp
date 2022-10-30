a54c4e36a564eb59fff2cb78f89cde49e6429b34
hossein rostami <rostamihossein1366@gmail.com>
2022-10-30 11:42:13 +0330

    first ObjectOrientedProgram

---
+++ FirstNameBirthDayObject/app/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+using IdentityClass;
+using Microsoft.VisualBasic;
+
+Person Adam = new()
+{
+    FirstName = "Adam",
+    LastName = "Green",
+    DateOfBirth = new DateTime(1366, 11, 19)
+};
+
+WriteLine($"{Adam.FirstName} {Adam.LastName}'s Birthday is on {Adam.DateOfBirth}");
---
+++ FirstNameBirthDayObject/object/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace IdentityClass;
+public class Person 
+{
+    public string FirstName;
+    public string LastName;
+    public DateTime DateOfBirth;
+}
