d6b213d860b848f4113e4f3a8cd5f8d4a5a43cd2
hossein rostami <rostamihossein1366@gmail.com>
2022-11-14 11:30:37 +0330

    first json

---
+++ json/json/post.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class post
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+    }
+
+
+}
